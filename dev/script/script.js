f6b2163d66805b8907c64b8e8ff6e23bc827c1e6
Parvez shaikh <parvezshaikhj@gmail.com>
2015-12-30 02:45:10 +0530

    Responsive changes updated sitewide

--- dev/script/script.js
+++ dev/script/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
-	
-	
+	$('.mobile-nav').on('click', function(){
+		$('nav ul').slideToggle(400, function(){
+			$(this).toggleClass('nav-expanded-menu').css('display','');
+		});
+	});
 });
